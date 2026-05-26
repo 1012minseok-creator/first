5a79f656a10537407a99c26187605c78259956a5
1012minseok-creator <1012minseok@gmail.com>
2026-05-26 14:01:05 +0900

    Add files via upload

---
+++ biz_st.py
@@ -0,0 +1,12 @@
+import streamlit as st
+st.title('첫번째 웹어플 만들기!!')
+"# 이건 부제목 #"
+"## 부제목 맞아? ##"
+'''### magic 적용 예시
+1. ordered item
+-강조: **unordered item**
+-기울임:*unordered item*
+-강조와 기울임 동시에:***unordered item***
+2. ordered item
+3. ordered item
+'''
